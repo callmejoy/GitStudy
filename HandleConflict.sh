416d228532534d1cde897a19091adab6a913ca9f
周毅 <zy2011019190023@gmail.com>
2015-10-23 21:23:29 +0800

    auto commit

--- HandleConflict.sh
+++ HandleConflict.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 rm ~/dump.rdb
 echo start dump REDIS_DATA
-scp root@111.13.101.208:/var/lib/redis/dump.rdb ~/
+scp root@CodeInDevBranch:/var/lib/redis/dump.rdb ~/
 
 ssh root@120.25.12.63 > /dev/null 2>&1 << eeooff
 redisPID=`pidof redis-server`
@@ -15,7 +15,7 @@ echo start upload REDIS_DATA
 scp ~/dump.rdb root@120.25.12.63:/var/lib/redis/
 rm ~/dump.rdb
 
-ssh root@somethingwronghere > /dev/null 2>&1 << eeooff
+
 chmod 777 /var/lib/redis/dump.rdb
 /etc/init.d/redis-server start
 exit
